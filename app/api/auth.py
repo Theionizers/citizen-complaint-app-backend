43253db0c9f978db174d003587a7010e32a844e2
the ionizers <pwallah69@gmail.com>
2026-08-18 22:28:49 +0530

    modified auth.py

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from app.core.security import hash_password
+from app.models import Role, User
 from app.db.database import get_db
+from app.models import User
 from app.schemas.auth import RegisterRequest
+
 
 router = APIRouter(
     prefix="/auth",
@@ -15,7 +18,47 @@ def register(
     data: RegisterRequest,
     db: Session = Depends(get_db)
 ):
+    existing_user = (
+        db.query(User)
+        .filter(User.email == data.email)
+        .first()
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=409,
+            detail="Email already registered"
+        )
+
+    citizen_role = (
+        db.query(Role)
+        .filter(Role.name == "citizen")
+        .first()
+    )
+
+    if citizen_role is None:
+        raise HTTPException(
+            status_code=500,
+            detail="Citizen role not configured"
+        )
+
+    password_hash = hash_password(data.password)
+
+    user = User(
+        name=data.name,
+        email=data.email,
+        password_hash=password_hash,
+        role_id=citizen_role.id
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
     return {
-        "message": "Registration endpoint working",
-        "email": data.email
+        "message": "User registered successfully",
+        "user_id": user.id,
+        "name": user.name,
+        "email": user.email,
+        "role": citizen_role.name
     }
