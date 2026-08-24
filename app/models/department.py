ee8492552aadf83bc295e13a4a074c778ba8a396
the ionizers <pwallah69@gmail.com>
2026-08-24 23:40:59 +0530

    added the department.py

---
+++ app/models/department.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+
+if TYPE_CHECKING:
+    from app.models import Service
+
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+
+    services: Mapped[list["Service"]] = relationship(
+        back_populates="department"
+    )
