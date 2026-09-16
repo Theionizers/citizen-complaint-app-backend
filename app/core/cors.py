2a948058e3f970407b897aba31f18fcbdb766477
the ionizers <pwallah69@gmail.com>
2026-09-16 16:44:12 +0530

    done all

--- app/core/cors.py
+++ app/core/cors.py
@@ -7,6 +7,7 @@ def setup_cors(app):
         allow_origins=[
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://citizen-app-frontend.vercel.app",
         ],
         allow_credentials=True,
         allow_methods=["*"],
